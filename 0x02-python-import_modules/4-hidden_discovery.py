4351e77d60c581fb420995554414c54f3f5738b9
Johnson Nzube <Senatornzubiz@gmail.com>
2022-12-07 20:43:00 -0800

    final

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from hidden_4 import *
-    array = dir()
-    for i in range(0, len(array)):
-        if array[i][0:2] != "__":
-            print("{}".format(array[i]))
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
